eca73803b086154602984d976e7d63d44d7182fb
Lucas Vieira <lvieirastos@gmail.com>
2021-01-13 20:31:47 -0300

    Projeto base finalizado

---
+++ app/src/main/java/com/example/minhasanotaes/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.minhasanotaes;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AnotacaoPreferencias preferencias;
+    private EditText editAnotacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editAnotacao = findViewById(R.id.editAnotacao);
+        preferencias = new AnotacaoPreferencias(getApplicationContext());
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String textoRecuperado = editAnotacao.getText().toString();
+
+                if (textoRecuperado.equals("")) {
+                    Snackbar.make(view, "Preencha a anotação", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+
+                } else {
+                    preferencias.salvarAnotacao(textoRecuperado);
+                    Snackbar.make(view, "Anotação salva com sucesso!", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+
+                }
+            }
+        });
+
+        // recuperar anotação
+        String anotacao = preferencias.recuperarAnotacao();
+        if (!anotacao.equals("")) {
+            editAnotacao.setText(anotacao);
+        }
+
+
+    }
+}
